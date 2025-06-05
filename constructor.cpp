9102b7d1e8c49f107012226e8e872cbd07d90fe1
AArusshi82 <143922485+AArusshi82@users.noreply.github.com>
2025-06-05 22:26:00 +0530

    parameterized constructor shown

---
+++ constructor.cpp
@@ -0,0 +1,39 @@
+// constructor is a special type of func with no return type
+//called as soon as an obj is created
+//memory is allocated on obj creation
+// constructor types: default and parameterized, default is called on its own as soon as the obj is created 
+//always declare constructor in public because main can only access public data and methods
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+class teacher{
+
+    private:
+string name;
+string dept;
+int salary;
+public:
+teacher(string n, string d, int s)
+{
+    name=n;
+    dept=d;
+    salary=s;
+}
+
+void getData()
+{
+    cout<<name<<endl<<dept<<endl<<salary;
+}
+};
+
+int main()
+{
+teacher t1("neesha","CSE",70000);
+t1.getData();
+
+
+
+}
